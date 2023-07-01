ef9414a1b7d784d7cdf691e53fa8e0f5aafb9c31
Nitish Mathur <108564972+mathur-nitish@users.noreply.github.com>
2023-07-01 09:52:19 +0530

    Add files via upload

---
+++ Build_Binary_Search_Tree.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+struct node {
+
+	int data;
+	node* left;
+	node* right;
+
+	node(int val) {
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+
+};
+
+
+node* insertBST(node* root, int val) {
+	if (root == NULL) {
+		return new node(val);
+	}
+	if (root->data > val) {
+		root->left = insertBST(root->left, val);
+	}
+	else if (root->data < val) {
+		root->right =  insertBST(root->right, val);
+	}
+
+	return root;
+}
+
+
+void inorder_traversal(node* root) {
+	if (root == NULL) {
+		return;
+	}
+
+	inorder_traversal(root->left);
+	cout << root->data << " ";
+	inorder_traversal(root->right);
+}
+
+void preorder_traversal(node* root) {
+	if (root == NULL) {
+		return;
+	}
+
+	cout << root->data << " ";
+	preorder_traversal(root->left);
+	preorder_traversal(root->right);
+}
+
+void postorder_traversal(node* root) {
+	if (root == NULL) {
+		return;
+	}
+
+	postorder_traversal(root->left);
+	postorder_traversal(root->right);
+	cout << root->data << " ";
+}
+
+int main() {
+
+	node* root = NULL;
+	root = insertBST(root, 5);
+	insertBST(root, 1);
+	insertBST(root, 3);
+	insertBST(root, 2);
+	insertBST(root, 4);
+	insertBST(root, 7);
+
+	cout << "In-order Traversal" << endl;
+	inorder_traversal(root);
+	cout << endl;
+
+
+	cout << "Pre-order Traversal" << endl;
+	preorder_traversal(root);
+	cout << endl;
+
+	cout << "Post-order Traversal" << endl;
+	postorder_traversal(root);
+	cout << endl;
+
+
+	return 0;
+}
